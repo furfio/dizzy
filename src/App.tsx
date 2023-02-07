46a4162eacaa8665daa743505dddb42e0b337514
duanzhenyu <duanzhenyu@microsoft.com>
2023-02-07 21:43:08 +0800

    add azure pipeline

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
 function App() {
-  return <h2>Hello dizzy!</h2>
+  return (
+    <>
+      <h2>hello world</h2>
+      <h2>hello dizzy</h2>
+    </>
+  )
 }
 export default App
